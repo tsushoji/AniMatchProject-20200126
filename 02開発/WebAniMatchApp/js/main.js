2e5ce70452fb6f134552046aa4211163b1de9f86
Shota Tsuji <s.tuji3007sho@gmail.com>
2020-02-21 00:44:30 +0900

    2020/02/21 seventh commit rocal

---
+++ 02開発/WebAniMatchApp/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $(".main-section-content-search").click(function () {
+        location.href = "search.html";
+    });
+
+    $(".main-section-content-message").click(function () {
+        location.href = "dmessage.html";
+    });
+
+});
