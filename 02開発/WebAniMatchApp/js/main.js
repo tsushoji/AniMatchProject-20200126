37846e2ff2071a4576189c0c0e4fa8f53a5b754c
Shota Tsuji <s.tuji3007sho@gmail.com>
2020-02-22 16:18:39 +0900

    2020/02/22 eighth commit rocal

--- 02開発/WebAniMatchApp/js/main.js
+++ 02開発/WebAniMatchApp/js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 
-    $(".main-section-content-search").click(function () {
-        location.href = "search.html";
+    $('.is-show--search').click(function () {
+        // 同じタグで表示
+        location.href = 'search.html';
     });
 
-    $(".main-section-content-message").click(function () {
-        location.href = "dmessage.html";
+    $('.is-show--dmessage').click(function () {
+        // 同じタグで表示
+        location.href = 'dmessage.html';
     });
+
+    $('.move-page--top').click(function() {
+        // スクロールの速度
+        var speed = 400; // ミリ秒
+        // 移動先を数値で取得
+        var position = $('html').offset().top;
+        // スムーススクロール
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+    });
+
 
 });
