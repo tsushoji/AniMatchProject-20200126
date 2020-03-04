10bcac6ace7c4cc35cd5607fe29408627c49a2d9
Shota Tsuji <s.tuji3007sho@gmail.com>
2020-03-04 23:25:21 +0900

    2020/03/04 ADD:新規登録画面作成 補足資料追加

--- 02開発/WebAniMatchApp/js/main.js
+++ 02開発/WebAniMatchApp/js/main.js
@@ -12,14 +12,5 @@ $(document).ready(function(){
         location.href = 'dmessage.html';
     });
 
-    $('.move-page--top').click(function() {
-        // スクロールの速度
-        var speed = 400; // ミリ秒
-        // 移動先を数値で取得
-        var position = $('html').offset().top;
-        // スムーススクロール
-        $('body,html').animate({scrollTop:position}, speed, 'swing');
-    });
-
 
 });
---
+++ 02開発/WebAniMatchApp/js/regist.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    //アクション:「登録区分」の値が変更される
+    $('#registtype').change(function() {
+        //登録区分の値を取得
+        var val = $(this).val();
+        //登録区分の値に応じて、formタグを表示・非表示にする
+        if(val == '1'){
+            //飼い主用フォームを表示
+            $('#owner-form').removeClass('hide');
+            $('#trimmer-form').addClass('hide');
+            // $('.footer-top').removeClass('hide');
+        }else if(val == '2'){
+            //トリマー用フォームを表示
+            $('#owner-form').addClass('hide');
+            $('#trimmer-form').removeClass('hide');
+            // $('.footer-top').removeClass('hide');
+        }else{
+            //フォームを非表示
+            $('#owner-form').addClass('hide');
+            $('#trimmer-form').addClass('hide');
+            // $('.footer-top').addClass('hide');
+        }   
+      });
+
+
+});
