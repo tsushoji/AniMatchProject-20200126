671c5d2f1134cf4553c5922a538e25f2216cf231
Shota Tsuji <s.tuji3007sho@gmail.com>
2020-03-17 20:56:23 +0900

    2020/03/17 UPDATE AND DELETE:再レビューご指摘点再修正

--- 02開発/WebAniMatchApp/js/commons/pageMove.js
+++ 02開発/WebAniMatchApp/js/commons/pageMove.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     //アクション:「上部へ戻る」をクリック
     $('.move-page-top').click(function() {
         // スクロールの速度
-        const speed = 400; // ミリ秒
+        const speedVal = 400;
+        let speed = speedVal; // ミリ秒
         // 移動先を数値で取得
         let position = $('html').offset().top;
         // スムーススクロール
--- 02開発/WebAniMatchApp/js/regist.js
+++ 02開発/WebAniMatchApp/js/regist.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
+    //初期表示時、フォームを非表示
+    $('.form-common,.form-owner,.form-trimmer,.form-trimmer-business-hours,.footer-top-content').hide();
 
     //アクション:「登録区分」の値が変更される
     $('#regist-type').change(function() {
-
         //飼い主区分
         const ownerType = '1';
         //トリマー区分
@@ -14,23 +15,15 @@ $(document).ready(function(){
         //登録区分の値に応じて、formタグを表示・非表示にする
         if(registType === ownerType){
             //飼い主用フォームを表示
-            $('.form-common').removeClass('hide');
-            $('.form-owner').removeClass('hide');
-            $('.form-trimmer').addClass('hide');
-            $('.footer-top-content').removeClass('hide');
+            $('.form-common,.form-owner,.footer-top-content').show();
+            $('.form-trimmer,.form-trimmer-business-hours').hide();
         }else if(registType === trimmerType){
             //トリマー用フォームを表示
-            $('.form-common').removeClass('hide');
-            $('.form-owner').addClass('hide');
-            $('.form-trimmer').removeClass('hide');
-            $('.footer-top-content').removeClass('hide');
+            $('.form-common,.form-trimmer,.footer-top-content').show();
+            $('.form-owner,.form-trimmer-business-hours').hide();
         }else{
             //フォームを非表示
-            $('.form-common').addClass('hide');
-            $('.form-owner').addClass('hide');
-            $('.form-trimmer').addClass('hide');
-            $('.footer-top-content').addClass('hide');
+            $('.form-common,.form-owner,.form-trimmer,.form-trimmer-business-hours,.footer-top-content').hide();
         }
       });
-
 });
