244a45344e3b34a6631edcb39167941cbcbe45fb
Shota Tsuji <s.tuji3007sho@gmail.com>
2020-04-12 00:55:52 +0900

    2020/04/12 ADD AND UPDATE:検索詳細画面作成 メイン画面、検索画面修正

--- 02開発/WebAniMatchApp/js/main.js
+++ 02開発/WebAniMatchApp/js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
 
     //アクション:「main-center-block」の検索ブロックをクリック
-    $('.is-show-search').click(function () {
+    $('.is-show-search-owner').click(function () {
         // 同じタグで表示
-        location.href = 'search.html';
+        location.href = 'search/search-owner.html';
+    });
+
+    //アクション:「main-center-block」の検索ブロックをクリック
+    $('.is-show-search-trimmer').click(function () {
+        // 同じタグで表示
+        location.href = 'search/search-trimmer.html';
     });
 
     //アクション:「main-center-block」のダイレクトメッセージブロックをクリック
